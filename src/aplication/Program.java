a20ed2802e24954a165d35c4afc401776f043da5
douglas <douglasgomesrodrigues902@gmail.com>
2020-09-10 12:00:07 -0300

    frist class position

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,13 @@
 package aplication;
 
+import Boardgame.position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		position pos = new position (3 , 5);
+		System.out.println(pos);
 	}
 
 }
