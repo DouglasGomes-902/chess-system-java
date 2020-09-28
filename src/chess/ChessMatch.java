611b4634b49799af0a5984693f7209758a401f65
douglas <douglasgomesrodrigues902@gmail.com>
2020-09-28 09:54:39 -0300

    King moviments

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -58,7 +58,7 @@ public class ChessMatch {
 	}
 	
 	public void validateTargetPosition(Position source , Position target) {
-		if (!board.piece(source).possibleMove(target)){
+		if (! board.piece(source).possibleMove(target)){
 			throw new ChessException("The chosen piece can't move to target position");
 			
 		}
