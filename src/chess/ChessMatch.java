7823386cc1e34feae674ea82fbc99a1b1f88ca05
douglas <douglasgomesrodrigues902@gmail.com>
2020-10-02 13:53:28 -0300

    Queen added

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Knight;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -221,6 +222,7 @@ public class ChessMatch {
 		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
+		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 
 		
 		//pawns black
@@ -240,6 +242,7 @@ public class ChessMatch {
 		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('b', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
+		placeNewPiece('d', 8, new Queen(board, Color.BLACK));
 
 		
 		
